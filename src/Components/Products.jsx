f3eb34a0d9e3f8df3d3ee1afd01e7c7a56a1fe51
Sounak Guin <sounak.guin@gmail.com>
2024-04-24 13:08:03 +0530

    Food Website

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -55,48 +55,45 @@ export default function Products({ cart, setcart, warning, setwarning }) {
 
   return (
     <div className="bg-gray-900">
-      <div className="my flex justify-between items-end px-4 sm:px-6 lg:px-8 flex-col md:flex-row">
-        <div className="flex relative mt-10 ml-3 md:ml-0">
-          <label className="text-white md:pt-1  md:text-xl">
-            Search by Food Name :&nbsp;
-          </label>
-          <input
-            type="text"
-            className="h-10 bg-white w-96 pr-8 pl-5 focus:shadow focus:outline-none rounded-lg text-black border-2 border-yellow-400 "
-            placeholder="Search with Item Name..."
-            value={searchTerm}
-            onChange={handelsearch}
-          />
-        </div>
-        <div className="relative mt-10 mr-36 md:mr-0 ">
-          <label className="text-white  md:text-xl">
-            Filter by Category :&nbsp;
-          </label>
-          <select
-            className="h-10 bg-white pr-8 pl-5 focus:shadow focus:outline-none rounded-lg text-black border-2 border-yellow-400"
-            value={filterOption}
-            onChange={handleFilterChange}
-          >
-            <option value="All">All</option>
-            <option value="Beef">Beef</option>
-            <option value="Chicken">Continental</option>
-            <option value="Seafood">Seafood</option>
-            <option value="Vegetarian">Vegetarian</option>
-            {/* Add more options as needed */}
-          </select>
-        </div>
-      </div>
+     <div className="my flex justify-center lg:justify-between lg:items-end flex-col lg:flex-row px-4 sm:px-6 lg:px-8">
+  <div className="flex relative mt-5 lg:mt-10 ml-3 lg:ml-0">
+    <label className="text-white lg:pt-1 lg:text-xl w-36 lg:w-auto  pt-1 ">Search by Name :&nbsp;</label>
+    <input
+      type="text"
+      className="h-10 bg-white w-80 lg:w-96 pr-8 pl-5 focus:shadow focus:outline-none rounded-lg text-black border-2 border-yellow-400 "
+      placeholder="Search with Item Name..."
+      value={searchTerm}
+      onChange={handelsearch}
+    />
+  </div>
+  <div className="flex  mt-5 lg:mt-10 ml-3 lg:ml-0 ">
+    <label className="text-white lg:text-xl w-36 lg:w-auto pt-1 lg:pt-1">Filter by Category :&nbsp;</label>
+    <select
+      className="h-10 bg-white w-60 lg:w-96 pr-8 pl-5 focus:shadow focus:outline-none rounded-lg text-black border-2 border-yellow-400"
+      value={filterOption}
+      onChange={handleFilterChange}
+    >
+      <option value="All">All</option>
+      <option value="Beef">Beef</option>
+      <option value="Chicken">Continental</option>
+      <option value="Seafood">Seafood</option>
+      <option value="Vegetarian">Vegetarian</option>
+      {/* Add more options as needed */}
+    </select>
+  </div>
+</div>
+
       {warning && (
-        <div className="text-white font-bold text-4xl pt-10 text-center">
+        <div className="text-white font-bold text-4xl pt-5 md:pt-10 text-center">
           Item is already present in your cart
         </div>
       )}
       <section className="text-gray-400 bg-gray-900 body-font mt-5">
-        <div className="px-5 py-10 mx-auto">
-          <div className="flex flex-wrap -m-4">
+        <div className="px-2 md:px-5 py-5 md:py-10 mx-auto">
+          <div className="flex flex-wrap -m-2 md:-m-4">
             {renderData &&
               renderData.map((item) => (
-                <div key={item.idMeal} className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                <div key={item.idMeal} className="lg:w-1/3 md:w-1/2 p-2 md:p-4 w-full">
                   <Link to={`/Noproductsata/${item.idMeal}`}>
                     <a className="block relative h-48 rounded-lg overflow-hidden">
                       <img
@@ -106,19 +103,20 @@ export default function Products({ cart, setcart, warning, setwarning }) {
                       />
                     </a>
                   </Link>
-                  <div className="flex items-center justify-between mt-4">
-                    <h2 className="text-white text-lg font-medium w-80 md:w-56">
-                      {item.strMeal}
+                  <div className="flex items-center justify-between mt-2 ">
+                    <h2 className="text-white text-lg font-medium w-2/3 md:w-3.6/4 lg:mt-0">
+                     Item name :&nbsp; {item.strMeal}
                     </h2>
                     <button
                       type="button"
-                      className="px-4 py-2 text-sm font-medium text-white bg-yellow-600 rounded-full hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+                      className="px-3 py-1 md:mt-2 lg:px-4 lg:py-3 lg:mt-4 text-sm font-medium text-white bg-yellow-600 rounded-full hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500"
                       onClick={() => handleaddtocart(item)}
                     >
                       Add To Cart
                     </button>
                   </div>
-                  <p>Price : 16$</p>
+                  <br/>
+                  <p className="text-gray-300 text-sm md:text-base">Price : 16$</p>
                 </div>
               ))}
           </div>
